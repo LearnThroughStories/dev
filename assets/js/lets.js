eb984a8fa7fac5eac151db5d787760979d2bba7d
sriram <sriram@malhar.net>
2021-06-01 17:54:53 +0530

    Added popup for sample book. Popup takes viewport aspect ratio into account so that the entire popup can fit into the viewport
    Bugs that need to be fixed: Popup needs to be centered on viewport,
    close button to be added.
    
    	new file:   assets/audio/00.mp3
    	new file:   assets/audio/01.mp3
    	new file:   assets/audio/02.mp3
    	new file:   assets/audio/03.mp3
    	new file:   assets/audio/04.mp3
    	new file:   assets/audio/05.mp3
    	new file:   assets/audio/06.mp3
    	new file:   assets/audio/07.mp3
    	new file:   assets/audio/08.mp3
    	new file:   assets/audio/09.mp3
    	new file:   assets/audio/10.mp3
    	new file:   assets/audio/11.mp3
    	new file:   assets/audio/12.mp3
    	new file:   assets/audio/13.mp3
    	new file:   assets/audio/14.mp3
    	new file:   assets/audio/15.mp3
    	new file:   assets/audio/16.mp3
    	new file:   assets/audio/17.mp3
    	new file:   assets/audio/18.mp3
    	new file:   assets/audio/19.mp3
    	new file:   assets/audio/20.mp3
    	new file:   assets/css/bookblock.css
    	modified:   assets/css/main.css
    	new file:   assets/img/book-sample/00.png
    	new file:   assets/img/book-sample/01.jpg
    	new file:   assets/img/book-sample/02.jpg
    	new file:   assets/img/book-sample/03.jpg
    	new file:   assets/img/book-sample/04.jpg
    	new file:   assets/img/book-sample/05.jpg
    	new file:   assets/img/book-sample/06.jpg
    	new file:   assets/img/book-sample/07.jpg
    	new file:   assets/img/book-sample/08.jpg
    	new file:   assets/img/book-sample/09.jpg
    	new file:   assets/img/book-sample/10.jpg
    	new file:   assets/img/book-sample/11.jpg
    	new file:   assets/img/book-sample/12.jpg
    	new file:   assets/img/book-sample/13.jpg
    	new file:   assets/img/book-sample/14.jpg
    	new file:   assets/img/book-sample/15.jpg
    	new file:   assets/img/book-sample/16.jpg
    	new file:   assets/img/book-sample/17.jpg
    	new file:   assets/img/book-sample/18.jpg
    	new file:   assets/img/book-sample/19.jpg
    	new file:   assets/img/book-sample/20.jpg
    	new file:   assets/js/jquery-3.5.1.min.js
    	new file:   assets/js/jquery.bookblock.js
    	new file:   assets/js/jquery.bookblock.min.js
    	new file:   assets/js/jquerypp.custom.js
    	new file:   assets/js/lets.js
    	new file:   assets/js/modernizr.custom.js
    	deleted:    img/bg-1200.jpg
    	deleted:    img/lets-logo-120.png
    	modified:   index.html

---
+++ assets/js/lets.js
@@ -0,0 +1,81 @@
+function Page() {
+  var config = {
+    $bookBlock: $("#bb-bookblock"),
+    $bookName: null, // will fix in init
+  };
+
+  var oldau = null;
+  var aualerted = false;
+
+  var playAudio = function (old, page, isLimit) {
+    if (isLimit) {
+      return false;
+    }
+    var url = page <= 9 ? "0" + page : "" + page;
+    url = "assets/audio/" + url + ".mp3";
+    var au = new Audio(url);
+    au.addEventListener("canplaythrough", (event) => {
+      if (oldau) {
+        oldau.pause();
+      }
+      au.play()
+        .then(function () {
+          oldau = au;
+        })
+        .catch(function (err) {
+          if (!aualerted) {
+            alert("Please enable automatic audio play");
+            aualerted = true;
+          }
+        });
+    });
+  }; // playAudio
+
+  var init = function (bookname) {
+      config.$bookName = bookname;
+      config.$bookBlock.bookblock({
+        speed: 300,
+        shadowSides: 0.8,
+        shadowFlip: 0.7,
+        onEndFlip: playAudio,
+      });
+      initEvents();
+      playAudio(0, 0, false); // Cover page
+    },
+    initEvents = function () {
+      var $slides = config.$bookBlock.children();
+      $slides.each(function (i) {
+        this.onclick = function (e) {
+          if (e.offsetX > e.target.width / 2) {
+            config.$bookBlock.bookblock("next");
+          } else {
+            config.$bookBlock.bookblock("prev");
+          }
+        };
+      });
+
+      // add keyboard events
+      $(document).keydown(function (e) {
+        var keyCode = e.keyCode || e.which,
+          arrow = {
+            left: 37,
+            up: 38,
+            right: 39,
+            down: 40,
+          };
+
+        switch (keyCode) {
+          case arrow.left:
+            config.$bookBlock.bookblock("prev");
+            break;
+          case arrow.right:
+            config.$bookBlock.bookblock("next");
+            break;
+        }
+      });
+    };
+
+  return { init: init };
+}
+
+window.Page = Page;
