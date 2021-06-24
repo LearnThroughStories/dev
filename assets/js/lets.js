5f53ba8680ff3e1059d89389038e59c9ca23f352
sriram <sriram@malhar.net>
2021-06-24 17:33:14 +0530

    De-duplicated code for book-open and put in lets.js

--- assets/js/lets.js
+++ assets/js/lets.js
@@ -115,3 +115,46 @@ $(".text-center").each(function(i, tw) {
     addMoreIndicatorIfOverflow({ target: tw });
   });
 });
+
+/* Support for book popup */
+
+function getViewportWidth() {
+    return Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+}
+function getViewportHeight() {
+    return Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+}
+const template = `
+<div id='bb-bookblock' class='bb-bookblock'>
+    <div id="book-close" onclick="closeBook()"> &times;<\/div>
+    <div class="bb-item"> <img src="assets/img/books/BOOKNAME/00.png" \/><\/div>
+    <div class="bb-item"> <img src="assets/img/books/BOOKNAME/01.jpg" \/><\/div>
+    <div class="bb-item"> <img src="assets/img/books/BOOKNAME/02.jpg" \/><\/div>
+    <div class="bb-item"> <img src="assets/img/books/BOOKNAME/03.jpg" \/><\/div>
+    <div class="bb-item"> <img src="assets/img/books/BOOKNAME/04.jpg" \/><\/div>
+<\/div>
+`
+
+function openBook(bookname) {
+    const book_open = $("#book-open");
+    book_open.css("display", "block");
+    const html = template.replaceAll("BOOKNAME", bookname);
+    book_open.append(html);
+    const bb_bookblock = book_open.find("#bb-bookblock");
+    if (getViewportWidth() < getViewportHeight()) {
+        bb_bookblock.addClass("bb-vw");
+    } else {
+        bb_bookblock.addClass("bb-vh");
+    }
+    const page = Page();
+    page.init(bookname);
+    book_open.find("#book-close").click(() => { closeBook(page) });
+}
+
+function closeBook(page) {
+    page.close();
+    const book_open = $("#book-open");
+    book_open.css("display", "none");
+    $("#book-open").empty();
+}
